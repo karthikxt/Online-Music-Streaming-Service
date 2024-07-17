b939131cd50da1001a8d19ae4429d1d56c502a0a
karthikeiyhan <karthikeiyhan@gmail.com>
2024-07-17 19:38:11 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,232 @@
+let allMusic = [
+  {
+    name: "Destiny",
+    artist: "Neffex",
+    img: "Destiny",
+    src: "Destiny"
+  },
+
+  {
+    name: "Grateful",
+    artist: "Neffex",
+    img: "Grateful",
+    src: "Grateful"
+  },
+  {
+    name: "Hope",
+    artist: "Neffex",
+    img: "Hope",
+    src: "Hope"
+  },
+  {
+    name: "Life",
+    artist: "Neffex",
+    img: "Life",
+    src: "Life"
+  },
+  {
+    name: "Rumors",
+    artist: "Neffex",
+    img: "Rumors",
+    src: "Rumors"
+  },
+  {
+    name: "Sorry",
+    artist: "Justin Bieber",
+    img: "Sorry",
+    src: "Sorry"
+  },
+  {
+    name: "Starboy",
+    artist: "Weeknd",
+    img: "Starboy",
+    src: "Starboy"
+  },
+  {
+    name: "Fight Back",
+    artist: "Neffex",
+    img: "FightBack",
+    src: "FightBack"
+  },
+  {
+    name: "Step On Up",
+    artist: "AianaGrande",
+    img: "SOP",
+    src: "SOP"
+  },
+  {
+    name: "Reminder",
+    artist: "Weeknd",
+    img: "Reminder",
+    src: "Reminder"
+  },
+  {
+    name: "Unforgettable",
+    artist: "French Motana",
+    img: "Unforgettable",
+    src: "Unforgettable"
+  },
+  {
+    name: "Wolves",
+    artist: "Selena Gomez",
+    img: "Wolves",
+    src: "Wolves"
+  },
+  {
+    name: "Harley Bird - Home",
+    artist: "Jordan Schor",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Ikson Anywhere - Ikson",
+    artist: "Audio Library",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Adiye",
+    artist: "GV Prakash",
+    img: "Adiye",
+    src: "Adiye"
+  },
+  {
+    name: "Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Alone",
+    artist: "Alan-Walker",
+    img: "Alone",
+    src: "Alone"
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name:"AfterHours",
+    artist: "Weeknd",
+    img: "AfterHours",
+    src: "AfterHours"
+  },
+  {
+    name: "Jim - Sun Goes Down",
+    artist: "Jim Yosef x Roy",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "music-6",
+    src: "music-6"
+  },
+ 
+  {
+    name: "Animals",
+    artist: "Maroon",
+    img: "Animals",
+    src: "Animals"
+  },
+  {
+    name: "Attention",
+    artist: "Charlie Puth",
+    img: "Attention",
+    src: "Attention"
+  },
+  {
+    name: "Blackbear",
+    artist: "Chill Nation",
+    img: "Blackbear",
+    src: "Blackbear"
+  },
+  {
+    name: "Blinding Lights",
+    artist: "Weeknd",
+    img: "BlindingLights",
+    src: "BlindingLights"
+  },
+  {
+    name: "Calm Down",
+    artist: "Rema",
+    img: "CalmDown",
+    src: "CalmDown"
+  },
+
+  {
+    name: "Closer",
+    artist: "The Chainsmokers",
+    img: "Closer",
+    src: "Closer"
+  },
+
+  {
+    name: "Blankspace",
+    artist: "TaylorSwift",
+    img: "Blankspace",
+    src: "Blankspace"
+  },
+
+  {
+    name: "Die For You",
+    artist: "Weeknd,Ariana-Grande",
+    img: "DieForYou",
+    src: "DieForYou"
+  },
+  
+  {
+    name: "Ghost",
+    artist: "Justin Bieber",
+    img: "Ghost",
+    src: "Ghost"
+  },
+
+  {
+    name: "Let Me Love You",
+    artist: "Dj-Snake",
+    img: "LetMeLoveYou",
+    src: "LetMeLoveYou"
+  },
+  {
+    name: "Where We Started",
+    artist: "Lost-Sky",
+    img: "WWS",
+    src: "WWS"
+  },
+  {
+    name: "My Baby Love Your Voice",
+    artist: "Jony",
+    img: "bby",
+    src: "bby"
+  },
+  {
+    name: "All These Thoughts",
+    artist: "Neffex",
+    img: "ATT",
+    src: "ATT"
+  },
+  {
+    name: "Careless",
+    artist: "Neffex",
+    img: "Careless",
+    src: "Careless"
+  },
+  {
+    name: "Chance",
+    artist: "Neffex",
+    img: "Chance",
+    src: "Chance"
+  },
+  {
+    name: "Cold",
+    artist: "Neffex",
+    img: "Cold",
+    src: "Cold"
+  },
+
+];
